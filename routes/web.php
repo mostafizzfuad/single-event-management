86eb5b9fbe55fedaa385f381b983e0ac2b2d2661
Mostafizur Rahman <mostafizzfuad@gmail.com>
2025-05-21 01:25:13 +0600

    admin template mastering done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminAuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// admin
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminAuthController::class, 'login'])->name('admin_login');
+    Route::get('/profile', [AdminAuthController::class, 'profile'])->name('admin_profile');
+    Route::get('/forget-password', [AdminAuthController::class, 'forget_password'])->name('admin_forget_password');
+    Route::get('/reset-password', [AdminAuthController::class, 'reset_password'])->name('admin_reset_password');
+
+    Route::get('/dashboard', [AdminDashboardController::class, 'dashboard'])->name('admin_dashboard');
 });

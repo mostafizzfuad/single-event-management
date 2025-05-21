fb6467b6773f581d07e8682befcf5fa01feb9308
Mostafizur Rahman <mostafizzfuad@gmail.com>
2025-05-21 14:03:06 +0600

    admin forget and reset password implemented

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Auth;
 use Hash;
 use App\Models\Admin;
+use App\Mail\Websitemail;
 
 class AdminAuthController extends Controller
 {
@@ -44,11 +45,58 @@ class AdminAuthController extends Controller
         return view('admin.profile');
     }
 
+    // forget password
     public function forget_password() {
         return view('admin.forget-password');
     }
 
-    public function reset_password() {
-        return view('admin.reset-password');
+    public function forget_password_submit(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+        ]);
+
+        $admin = Admin::where('email', $request->email)->first();
+        if (!$admin) {
+            return redirect()->back()->with('error', 'Email is not found');
+        }
+
+        $token = hash('sha256', time());
+        $admin->token = $token;
+        $admin->update();
+
+        $reset_link = url('admin/reset-password/' . $token . '/' . $request->email);
+        $subject = "Password Reset";
+        $message = "To reset password, please click on the link below:<br>";
+        $message .= "<a href='" . $reset_link . "'>Click Here</a>";
+
+        \Mail::to($request->email)->send(new Websitemail($subject, $message));
+
+        return redirect()->back()->with('success', 'We have sent a password reset link to your email. Please check your email. If you do not find the email in your inbox, please check your spam folder.');
+    }
+
+    // reset password
+    public function reset_password($token, $email)
+    {
+        $admin = Admin::where('email', $email)->where('token', $token)->first();
+        if (!$admin) {
+            return redirect()->route('admin_login')->with('error', 'Token or email is not correct');
+        }
+        return view('admin.reset-password', compact('token', 'email'));
+    }
+
+    public function reset_password_submit(Request $request, $token, $email)
+    {
+        $request->validate([
+            'password' => ['required'],
+            'confirm_password' => ['required', 'same:password'],
+        ]);
+
+        $admin = Admin::where('email', $request->email)->where('token', $request->token)->first();
+        $admin->password = Hash::make($request->password);
+        $admin->token = "";
+        $admin->update();
+
+        return redirect()->route('admin_login')->with('success', 'Password reset is successful. You can login now.');
     }
 }
---
+++ resources/views/email.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Email</title>
+</head>
+
+<body>
+    {!! $body !!}
+</body>
+
+</html>
